843a40e736158c58fc269fb7ce2e3d4ea1eb55b1
Ordinary33 <husnie.baddie@g.msuiit.edu.ph>
2026-01-08 00:06:15 +0800

    feat: Add api request and print the output.

--- streamlit_app/pages/1_📚_Books.py
+++ streamlit_app/pages/1_📚_Books.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import requests
 
 st.title("Book Recommender")
+
+isbn = st.text_input("Enter ISBN of a book")
+
+
+if st.button("Get Recommendations"):
+    if isbn:
+        try:
+            response = requests.get(f"http://127.0.0.1:8000/recommend/{isbn}")
+            response.raise_for_status()
+            result = response.json()
+
+            with st.container():
+                st.subheader("Recommendations:")
+                st.write(result)
+
+        except requests.exceptions.HTTPError as err:
+            st.error(f"Book not found or API error : {err}")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+    else:
+        st.warning("Please enter a valid ISBN.")
