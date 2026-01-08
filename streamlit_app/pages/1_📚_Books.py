a7b53129fa466c70031aa160336d14d91549902a
Ordinary33 <husnie.baddie@g.msuiit.edu.ph>
2026-01-08 21:00:48 +0800

    feat: Improve book recommendations UI and error handling.

--- streamlit_app/pages/1_📚_Books.py
+++ streamlit_app/pages/1_📚_Books.py
@@ -9,15 +9,34 @@ isbn = st.text_input("Enter 10-digit ISBN of a book")
 if st.button("Get Recommendations"):
     if isbn:
         try:
-            response = requests.get(f"http://127.0.0.1:8000/recommend/{isbn}")
-            response.raise_for_status()
-            result = response.json()
+
+            with st.spinner("Fetching recommendations..."):
+                response = requests.get(f"http://127.0.0.1:8000/recommend/{isbn}")
+                response.raise_for_status()
+                result = response.json()
+
             recommendations = result.get("data", [])
+            title = result.get("title", "The Book")
 
-            st.subheader(f"Recommendations for {result.get('title', 'the book')}:")
+            if not recommendations:
+                st.warning(f"No recommendations found for ISBN {isbn}.")
 
-            with st.container():
-                st.write(result)
+            else:
+                st.write(f"**Recommendations based on '{title}':**")
+                with st.container():
+                    for data in recommendations:
+                        st.markdown("---")
+                        col1, col2 = st.columns([1, 3])
+                        with col1:
+                            st.image(data["Image"], width=100)
+                        with col2:
+                            st.markdown(f"**Title:** {data['Title']}")
+                            st.markdown(f"**Author:** {data['Author']}")
+                            st.markdown(f"**Year:** {data['Year']}")
+                            st.markdown(f"**ISBN:** {data['ISBN']}")
+                    st.markdown("---")
+                    st.markdown("Json Response:")
+                    st.json(result)
 
         except requests.exceptions.HTTPError:
             st.error(f"Book with the isbn {isbn} not found in the database")
